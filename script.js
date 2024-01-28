ce61b632a77f9024faf932f8d766fe0fd2e22f66
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-01-28 23:56:36 +0530

    1:50:02

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href);
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
     return songs;
@@ -18,6 +18,19 @@ async function getSongs(){
 async function main(){
     let songs = await getSongs();
     console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20"," ")} </li>`;
+    }
+
+    var audio = new Audio(songs[1]);
+    //audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(duration);
+      });
 }
 
 main();
